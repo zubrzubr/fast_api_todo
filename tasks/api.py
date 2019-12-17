d06ea2d3b42192debf98cabf39ad5567bc680c2c
zubrzubr <maxzub@gmail.com>
2019-12-17 22:54:32 +0200

    Add new api points

--- tasks/api.py
+++ tasks/api.py
@@ -10,9 +10,19 @@ service = TaskService()
 
 @router.get("/tasks/{item_id}")
 async def read_item(item_id: int):
-    return {"item_id": item_id}
+    return service.get_item(item_id)
+
+
+@router.put("/tasks/{item_id}")
+async def update_item(item_id: int):
+    return service.update_item(item_id)
 
 
 @router.post("/tasks/")
 async def add_item(item: Item):
     return service.add_item(item)
+
+
+@router.delete("/tasks/{item_id}")
+async def update_item(item_id: int):
+    return service.delete_item(item_id)
