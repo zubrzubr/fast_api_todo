21c824e44339ebab48ae5d0f0dae854f796a43e2
zubrzubr <maxzub@gmail.com>
2020-01-10 23:05:01 +0200

    Enhance category scheme

--- category/schemas.py
+++ category/schemas.py
@@ -8,3 +8,14 @@ from task.schemas import Task
 class CategoryBase(BaseModel):
     name: str
     tasks: List[Task] = []
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class Category(CategoryBase):
+    id = int
+
+    class Config:
+        orm_mode = True
